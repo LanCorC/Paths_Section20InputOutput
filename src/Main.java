d829ef78e4e5b96706ceebecbbc8bd30085fc8c2
Lance <lanc3.corcega@gmail.com>
2023-08-01 17:53:09 +1200

    Section 20.338(JavaFX; Exists and CopyFile): introduced to static Files (.nio) static methods, .exists(), permission checks .isReadable(), .isExecutable(), .isWritable(), and Files.copy(path1, path2);

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,56 @@ import java.nio.file.*;
 
 public class Main {
     public static void main(String[] args) {
-        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
-        printFile(path);
-
-//        Path filePath = FileSystems.getDefault().getPath("files\\SubdirectoryFile.txt");
-        Path filePath = Paths.get(".\\files\\SubdirectoryFile.txt");
-        printFile(filePath);
-
-        //Note: below includes special file outside the working directory
-        //Note2: this absolute path is also for one device
-//        filePath = Paths.get("C:\\Users\\lance\\Documents\\CodingJava\\OutThere.txt");
-//        printFile(filePath);
-
-        filePath = Paths.get(".");
-        System.out.println(filePath.toAbsolutePath());
-
-        Path path2 = FileSystems.getDefault().getPath(".\\files\\..\\files\\SubdirectoryFile.txt");
-        System.out.println(path2.normalize().toAbsolutePath());
-        printFile(path2.normalize());
-    }
-
-    private static void printFile(Path path) {
-        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
-            String line;
-            while((line = fileReader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        try {
+            Path sourceFile = FileSystems.getDefault().getPath("Examples\\file1.txt");
+            Path copyFile = FileSystems.getDefault().getPath("Examples\\file1copy.txt");
+            Files.copy(sourceFile, copyFile);
+        } catch(IOException e) {
             e.printStackTrace();
         }
     }
+
+//        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+//        printFile(path);
+//
+////        Path filePath = FileSystems.getDefault().getPath("files\\SubdirectoryFile.txt");
+//        Path filePath = Paths.get(".\\files\\SubdirectoryFile.txt");
+//        printFile(filePath);
+//
+//        //Note: below includes special file outside the working directory
+//        //Note2: this absolute path is also for one device
+////        filePath = Paths.get("C:\\Users\\lance\\Documents\\CodingJava\\OutThere.txt");
+////        printFile(filePath);
+//
+//        filePath = Paths.get(".");
+//        System.out.println(filePath.toAbsolutePath());
+//
+//        Path path2 = FileSystems.getDefault().getPath(".\\files\\..\\files\\SubdirectoryFile.txt");
+//        System.out.println(path2.normalize().toAbsolutePath());
+//        printFile(path2.normalize());
+//
+//        Path path3 = FileSystems.getDefault().getPath("thisfiledoesntexist.txt");
+//        System.out.println(path3.toAbsolutePath());
+//
+//        Path path4 = Paths.get("./directory/doesnt/exist.txt");
+//        System.out.println(path4.toAbsolutePath());
+//
+//        filePath = FileSystems.getDefault().getPath("files");
+//        System.out.println("Exists = " + Files.exists(filePath));
+//
+//        System.out.println("Exists = " + Files.exists(path4));
+//    }
+//
+//    private static void printFile(Path path) {
+//        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+//            String line;
+//            while((line = fileReader.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
 }
