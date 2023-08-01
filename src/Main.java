59cc905abee1f777642986ce986b660a3bf5318b
Lance <lanc3.corcega@gmail.com>
2023-08-01 17:00:02 +1200

    Section 20.337(JavaFX; More on Paths): introduced "dot" for current directory, "dot dot" for parent directory, path.toAbsolute() to avoid confusion,

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,21 @@ public class Main {
         Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
         printFile(path);
 
-        Path filePath = FileSystems.getDefault().getPath("files\\SubdirectoryFile.txt");
+//        Path filePath = FileSystems.getDefault().getPath("files\\SubdirectoryFile.txt");
+        Path filePath = Paths.get(".\\files\\SubdirectoryFile.txt");
         printFile(filePath);
 
-        filePath = Paths.get("C:\\Users\\lance\\Documents\\CodingJava\\OutThere.txt");
-        printFile(filePath);
+        //Note: below includes special file outside the working directory
+        //Note2: this absolute path is also for one device
+//        filePath = Paths.get("C:\\Users\\lance\\Documents\\CodingJava\\OutThere.txt");
+//        printFile(filePath);
+
+        filePath = Paths.get(".");
+        System.out.println(filePath.toAbsolutePath());
+
+        Path path2 = FileSystems.getDefault().getPath(".\\files\\..\\files\\SubdirectoryFile.txt");
+        System.out.println(path2.normalize().toAbsolutePath());
+        printFile(path2.normalize());
     }
 
     private static void printFile(Path path) {
