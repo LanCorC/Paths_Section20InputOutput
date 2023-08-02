295eecdd045c87de482c82079c74f4409133dd72
LanCorC <lanc3.corcega@gmail.com>
2023-08-02 18:33:13 +1200

    Section 20.339(JavaFX; Move, Rename and Delete): introduction to Files.copy() and .move(), as well as optional third parameter StandardCopyOption.REPLACE_EXISTING; introduced to Files.delete() and .deleteIfExists()

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Path sourceFile = FileSystems.getDefault().getPath("Examples\\file1.txt");
-            Path copyFile = FileSystems.getDefault().getPath("Examples\\file1copy.txt");
-            Files.copy(sourceFile, copyFile);
+            Path fileToDelete = FileSystems.getDefault().getPath("Examples\\Dir1\\file1copy.txt");
+            Files.deleteIfExists(fileToDelete);
+
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples\\file1.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples\\file2.txt");
+//            Files.move(fileToMove, destination);
+
+//            Path sourceFile = FileSystems.getDefault().getPath("Examples\\file1.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("Examples\\file1copy.txt");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//
+//            sourceFile = FileSystems.getDefault().getPath("Examples\\Dir1");
+//            copyFile = FileSystems.getDefault().getPath("Examples\\Dir4");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+
         } catch(IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 
